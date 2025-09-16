447cd235d6f2d8f1901bd36fbdc57f7dc95b8150
JoeAaron09 <josephaarongrobles19@gmail.com>
2025-09-15 22:49:27 -0500

    Add files via upload
     Carga de la sección del problema 2

---
+++ Problema2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace JeremyR_JosephG_DanielC_Lab3
+{
+    partial class Problema2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbDefinirRango = new System.Windows.Forms.Label();
+            this.tbRango = new System.Windows.Forms.TextBox();
+            this.lbPalindromos = new System.Windows.Forms.ListBox();
+            this.btProb3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbDefinirRango
+            // 
+            this.lbDefinirRango.AutoSize = true;
+            this.lbDefinirRango.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDefinirRango.ForeColor = System.Drawing.Color.SeaShell;
+            this.lbDefinirRango.Location = new System.Drawing.Point(28, 36);
+            this.lbDefinirRango.Name = "lbDefinirRango";
+            this.lbDefinirRango.Size = new System.Drawing.Size(409, 25);
+            this.lbDefinirRango.TabIndex = 0;
+            this.lbDefinirRango.Text = "Defina un rango de números palíndromos:";
+            // 
+            // tbRango
+            // 
+            this.tbRango.BackColor = System.Drawing.Color.DarkRed;
+            this.tbRango.Font = new System.Drawing.Font("Franklin Gothic Book", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRango.ForeColor = System.Drawing.Color.MistyRose;
+            this.tbRango.Location = new System.Drawing.Point(30, 88);
+            this.tbRango.MaxLength = 25;
+            this.tbRango.Name = "tbRango";
+            this.tbRango.Size = new System.Drawing.Size(407, 27);
+            this.tbRango.TabIndex = 1;
+            this.tbRango.TextChanged += new System.EventHandler(this.tbRango_TextChanged);
+            // 
+            // lbPalindromos
+            // 
+            this.lbPalindromos.BackColor = System.Drawing.Color.Firebrick;
+            this.lbPalindromos.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPalindromos.ForeColor = System.Drawing.Color.MistyRose;
+            this.lbPalindromos.FormattingEnabled = true;
+            this.lbPalindromos.ItemHeight = 25;
+            this.lbPalindromos.Location = new System.Drawing.Point(41, 157);
+            this.lbPalindromos.Name = "lbPalindromos";
+            this.lbPalindromos.Size = new System.Drawing.Size(358, 179);
+            this.lbPalindromos.TabIndex = 4;
+            // 
+            // btProb3
+            // 
+            this.btProb3.BackColor = System.Drawing.Color.Maroon;
+            this.btProb3.Font = new System.Drawing.Font("Franklin Gothic Book", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btProb3.ForeColor = System.Drawing.Color.SeaShell;
+            this.btProb3.Location = new System.Drawing.Point(345, 386);
+            this.btProb3.Name = "btProb3";
+            this.btProb3.Size = new System.Drawing.Size(141, 38);
+            this.btProb3.TabIndex = 5;
+            this.btProb3.Text = "Problema 3";
+            this.btProb3.UseVisualStyleBackColor = false;
+            this.btProb3.Click += new System.EventHandler(this.btProb3_Click);
+            // 
+            // Problema2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btProb3);
+            this.Controls.Add(this.lbPalindromos);
+            this.Controls.Add(this.tbRango);
+            this.Controls.Add(this.lbDefinirRango);
+            this.Name = "Problema2";
+            this.Text = "Problema 2";
+            this.Load += new System.EventHandler(this.Problema2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDefinirRango;
+        private System.Windows.Forms.TextBox tbRango;
+   
+        private System.Windows.Forms.ListBox lbPalindromos;
+        private System.Windows.Forms.Button btProb3;
+    }
+}
---
+++ Problema2.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeremyR_JosephG_DanielC_Lab3
+{
+    //Jeremy Rodriguez, Joseph Guerra, Daniel Comrie
+    public partial class Problema2 : Form
+    {
+        public Problema2()
+        {
+            InitializeComponent();
+        }
+
+        private void Problema2_Load(object sender, EventArgs e)
+        {
+            btProb3.Enabled = false;
+            lbPalindromos.Items.Clear();
+
+            tbRango.KeyDown += tbRango_KeyDown;
+        }
+
+        private void tbRango_TextChanged(object sender, EventArgs e)
+        {
+            VerificarCampos();
+        }
+
+        private void tbRango_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                ListarPalindromos();
+            } 
+        }
+
+        private void ListarPalindromos()
+        {
+            lbPalindromos.Items.Clear();
+
+            if(!TryParseRange(tbRango.Text, out int from, out int to, out string error))
+            {
+                MessageBox.Show(error, "Rango incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            for (int n = from; n <= to; n++)
+                if (VerificarPalindromo(n))
+                    lbPalindromos.Items.Add(n);
+            if (lbPalindromos.Items.Count == 0)
+                lbPalindromos.Items.Add("No hay palíndromos en el rango presentado");
+        }
+
+        void VerificarCampos()
+        {
+            btProb3.Enabled = !string.IsNullOrWhiteSpace(tbRango.Text);
+        }
+
+        private void btProb3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private bool TryParseRange(string input, out int from, out int to, out string error)
+        {
+            from = 0;
+            to = 0;
+            error = null;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                error = "Escriba un rango válido";
+                return false;
+            }
+            string cadena = input.Trim();
+            if (cadena.Contains("-"))
+            {
+                var partes = cadena.Split('-');
+                if (partes.Length != 2 || !int.TryParse(partes[0].Trim(), out from) 
+                || !int.TryParse(partes[1].Trim(), out to))
+                {
+                    error = "Formato incorrecto. Debe ingresar variable a - variable b";
+                    return false;
+                }
+            }
+            else
+            {
+                if (!int.TryParse(cadena, out to))
+                {
+                    error = "Formato incorrecto. Debe ingresar un rango entre 2 números";
+                    return false;
+                }
+                from = 0;
+            }
+            if (from < 0 || to < 0)
+            {
+                error = "Incorrecto. No se permiten números negativos";
+                return false;
+            }
+            if (from > to)
+            {
+                error = "Incorrecto. El primer valor no puede ser mayor al segundo";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool VerificarPalindromo (int num)
+        {
+            string s = num.ToString();
+            int i = 0, j = s.Length - 1;
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                    return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
